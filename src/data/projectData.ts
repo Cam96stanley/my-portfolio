9bdddf063f7fa532d6298920f1920e7f67f5524f
Cameron Stanley <cam96stanley@gmail.com>
2026-05-19 14:21:55 -0400

    feat: added projects to project page

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,17 +1,17 @@
 export const projectData = [
   {
-    badge: "featured",
+    badge: "Featured",
     title: "Pet Foundation Website",
     description:
       "A pet foundation website built with Astro, TypeScript, and Tailwind",
     techStack: ["Astro", "TypeScript", "Tailwind"],
     year: "2026",
-    category: "frontend",
+    category: "Frontend",
     href: "https://edmaf-webapp.vercel.app/",
     thumbnail: "/edmaf.webp",
   },
   {
-    badge: "featured",
+    badge: "Featured",
     title: "Humane Society Website",
     description:
       "A humane society website built with Next, TypeScript, Tailwind, Zod, React Hook Forms, and Prisma",
@@ -29,14 +29,36 @@ export const projectData = [
     thumbnail: "/nw-montana.webp",
   },
   {
-    badge: "featured",
+    badge: "Featured",
     title: "Faith based Non-Profit Website",
     description:
       "A faith based non-profit website built with Next, TypeScript, and Tailwind",
     techStack: ["Next", "TypeScript", "Tailwind"],
     year: "2026",
-    category: "frontend",
+    category: "Frontend",
     href: "#",
     thumbnail: "/fotn.webp",
   },
+  {
+    badge: "Dashboard",
+    title: "Bookmark Manager",
+    description:
+      "A web bookmark app built with Next, TypeScript, Tailwind, and Prisma",
+    techStack: ["Next", "TypeScript", "Tailwind", "Zod", "Prisma"],
+    year: "2026",
+    category: "Frontend / Backend",
+    href: "https://bookmark-app-gray-three.vercel.app/",
+    thumbnail: "/bookmark-app.webp",
+  },
+  {
+    badge: "Dashboard",
+    title: "FinDash Finance Dashboard",
+    description:
+      "A finance dashboard built with Next, TypeScript, Tailwind, Drizzle, Clerk, Hono, and Turso",
+    techStack: ["Next", "TypeScript", "Tailwind", "Drizzle", "Clerk", "Hono", "Turso"],
+    year: "2026",
+    category: "Frontend / Backend",
+    href: "https://finance-app-eight-ochre.vercel.app/",
+    thumbnail: "/findash.webp",
+  }
 ];
